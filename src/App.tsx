2504a14f1a978c88dab456d4d43c03963508028d
Michael <mishchenko.mykhailo@gmail.com>
2023-02-02 02:46:11 +0200

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
 	return (
 		<div className="bg-slate-600 dark:bg-amber-300 px-4 w-full">
-			<h1>Helo World</h1>
+			<h1>Hello World</h1>
 			<h2 className="my-10 dark:divide-lime-500">Hello tailwind</h2>
 		</div>
 	);
